fe637a665224dccf9b39b579d8f1eaa9b4f2ed22
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-02 16:29:31 +0100

    btnGenerator click

--- js/main.js
+++ js/main.js
@@ -3,14 +3,17 @@ const btnGenerator = document.getElementById("btn-generator");
 const boardContainer = document.querySelector(".board");
 
 boardContainer.innerHTML = "";
-
-for( let i = 1; i <= 100; i++){
-    const boardCell = document.createElement("div");
-    boardCell.innerHTML = i;
-    boardCell.classList.add("board__number");
-    boardCell.addEventListener("click", function(){
-        console.log(this.innerHTML);
-        boardCell.classList.add("lightblue");
-    })
-    boardContainer.append(boardCell);
+btnGenerator.addEventListener('click', function(){
+    
+    for( let i = 1; i <= 100; i++){
+        const boardCell = document.createElement("div");
+        boardCell.innerHTML = i;
+        boardCell.classList.add("board__number");
+        boardCell.addEventListener("click", function(){
+            console.log(this.innerHTML);
+            boardCell.classList.add("lightblue");
+        })
+        boardContainer.append(boardCell);
+    }
 }
+
