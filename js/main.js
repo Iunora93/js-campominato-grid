deb73fcd74e7a0e377bcb8c27341733cb49ae73f
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-02 15:37:41 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+`use strict`
+const boardContainer = document.querySelector(".board");
+
+boardContainer.innerHTML = "";
+for( let i = 1; i <= 100; i++){
+    const boardCell = document.createElement("div");
+    boardCell.innerHTML = i;
+    boardCell.classList.add("board__number");
+    boardCell.addEventListener("click", function(){
+        console.log(this.innerHTML);
+        boardCell.classList.add("lightblue");
+    })
+    boardContainer.append(boardCell);
+}
